9190090c956fd8f208a14c7671c1b3ff8b09f8b7
lukersmithoscar <113738417+lukersmithoscar@users.noreply.github.com>
2023-08-22 14:41:03 +1000

    First migration to module

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 from datetime import datetime
-import requests
 from requests.adapters import HTTPAdapter
 from requests.exceptions import RetryError
 from urllib3.util.retry import Retry
-import time
+from utils.request_util import make_request, get_paths_or_empty
 import json
 import re
 
@@ -62,17 +61,6 @@ def retry(exception, tries=10, delay=0.5, backoff=2):
         return wrapper
 
     return decorator_retry
-
-def make_request(endpoint, querystring='', method='GET', **request_kwargs):
-    time.sleep(0.5)  # Rate limit: 3 requests per second
-    headers = {
-        'Authorization': f'Bearer {NOTION_TOKEN}',
-        'Content-Type': 'application/json',
-        'Notion-Version': '2022-02-22'
-    }
-    url = f'https://api.notion.com/v1/{endpoint}{querystring}'
-    resp = requests.request(method, url, headers=headers, **request_kwargs)
-    return resp.json()
 
 def create_database():
     children_query_resp = make_request(
@@ -436,20 +424,6 @@ def create_record(database_id, model_name, data, catalog_nodes):
             method='POST',
             json=record_obj
         )
-
-def get_paths_or_empty(parent_object, paths_array, zero_value=''):
-  """Used for catalog_nodes accesses, since structure is variable"""
-  for path in paths_array:
-    obj = parent_object
-    for el in path:
-      if el not in obj:
-        obj = zero_value
-        break
-      obj = obj[el]
-    if obj != zero_value:
-      return obj
-
-  return zero_value
 
 def get_owner(data, catalog_nodes, model_name):
   """
---
+++ utils/request_util.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+def make_request(endpoint, querystring='', method='GET', **request_kwargs):
+    time.sleep(0.5)  # Rate limit: 3 requests per second
+    headers = {
+        'Authorization': f'Bearer {NOTION_TOKEN}',
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-02-22'
+    }
+    url = f'https://api.notion.com/v1/{endpoint}{querystring}'
+    resp = requests.request(method, url, headers=headers, **request_kwargs)
+    return resp.json()
+
+
+def get_paths_or_empty(parent_object, paths_array, zero_value=''):
+  """Used for catalog_nodes accesses, since structure is variable"""
+  for path in paths_array:
+    obj = parent_object
+    for el in path:
+      if el not in obj:
+        obj = zero_value
+        break
+      obj = obj[el]
+    if obj != zero_value:
+      return obj
+
+  return zero_value
